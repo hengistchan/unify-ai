dbf6936156a7fd475517191411ff7874c6bb1b31
hengistchan <hengistchan>
2026-02-17 23:46:40 +0800

    fix: adjust toast position to top-16

--- packages/gui/src/App.tsx
+++ packages/gui/src/App.tsx
@@ -28,7 +28,7 @@ function App() {
         </Routes>
       </Layout>
       {/* Toast notifications */}
-      <div className="fixed top-24 right-4 z-[100] flex flex-col gap-2">
+      <div className="fixed top-16 right-4 z-[100] flex flex-col gap-2">
         {toasts.map((toast: ToastType) => (
           <Toast
             key={toast.id}
